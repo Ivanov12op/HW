cea523e1cf3d07b38a3284e967306fadd4e2b8f6
Ivanov12op <100442818+Ivanov12op@users.noreply.github.com>
2022-03-16 18:30:21 +0200

    Add files via upload

---
+++ Home Work/lab8/pe.py
@@ -0,0 +1,40 @@
+
+##task1:
+#try:
+   # number=int(input(' ''\n'' ''\n'' ''\n'' '))
+   # if number==0:
+      #  pass  
+#except:
+   # number=int(input(" ",' ',' ',' ',' ',' '))
+    
+  #todo
+
+
+    
+x1=int(input(" "))
+x2=int(input(" "))
+x3=int(input(" "))
+x4=int(input(" "))
+x5=int(input(" "))
+x6=int(input(" "))
+
+x7=int(input(" "))
+x8=int(input(" "))
+number_n=(x1,x2,x3,x4,x5,x6,x7,x8)
+numbers=list(number_n)
+   ##TODO
+#try:
+    #len in l1<4
+#except:
+    #print(f'Enter mor{" "} please')
+l1= sorted(numbers)
+
+for x in l1:
+   if x==0:
+        continue
+sor_numbers1=(l1[2:6])
+
+print(sor_numbers1)
+print(l1)
+
+
