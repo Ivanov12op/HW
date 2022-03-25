5a8a66bac4815f67b7362a6fd9723d3379fca7bc
Ivanov12op <100442818+Ivanov12op@users.noreply.github.com>
2022-03-25 14:45:47 +0200

    Update pmt.py

--- Home Work/lab9/pmt.py
+++ Home Work/lab9/pmt.py
@@ -14,6 +14,33 @@ if 0<=num<99:
                  
      
       print(number.dict)
+      
+ # labs9
+#задача 3:
+
+Menu ={ 'fish' 'pk' 'cake' 'beer' }
+
+
+Menu['fish' ] = 7
+Menu['pk'] = 2
+Menu['cake' ] = 3
+Menu['beer' ] = 2
+Current  = 0
+
+while True:
+   Menu=input( 'Please enter your order:" "  ')
+   for price in Menu.value( ):
+       Current= (Menu.sum[price])
+   for order in Menu.keys():
+       order=( order )
+   Current_order=(f'Yours order is  {order} = {Current} lv.')
+while False:
+   print(f'Sorry, we are out {' '}  todey. Order {Current} ')
+      f x in  Current==" ":
+   print(f'Your total is { Curent } lv' )
+
+####TODO   !!!!!
+
 
 
 
