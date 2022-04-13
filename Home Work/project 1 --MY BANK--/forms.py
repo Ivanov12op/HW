11f80a03ec83a3446413eedd39b13f7de524805d
Ivanov12op <100442818+Ivanov12op@users.noreply.github.com>
2022-04-13 14:05:17 +0300

    Add files via upload

---
+++ Home Work/project 1 --MY BANK--/forms.py
@@ -0,0 +1,11 @@
+def form1():
+    form1=  (f" { '=' *80 }\n{ '=' *80 } ")
+    print(form1)
+def form2():
+    form2_ =  (f" ! {  30 * '-' }  Welcome to MY Bank $$$  { '-' * 30 } ! ")
+    print(form2) 
+def form3():
+    form3_= (f" { 80 * '*' }")
+    return
+
+print(form1)
---
+++ Home Work/project 1 --MY BANK--/tmp.py
@@ -0,0 +1,16 @@
+
+import forms
+print(forms.form1)
+
+#users=()
+
+#x=None
+#while True :
+    
+    #try:
+      # user=input('Please Input Fullname (ex. Ada Jochn) : ')
+      # users =user.append (x)
+   # except:
+       #print ('Please enter a valid Fullname !!! ')
+       
+    
